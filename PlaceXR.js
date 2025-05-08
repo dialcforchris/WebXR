0c2713fa3a2a029f097c7b5b1a5b1c0d0c2e4c0d
DialC <mrchrismugridge@gmail.com>
2025-05-08 23:59:35 +0100

    Update PlaceXR.js

--- PlaceXR.js
+++ PlaceXR.js
@@ -1,7 +1,7 @@
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader';
+//import { DRACOLoader } from 'three/addons/loaders/DRACOLoader';
 // import { XRWebGLLayer } from 'webxr'; // Import the XRWebGLLayer class
 const models = ["./minaturePortrait.gltf",
      "./TestModel.gltf",
@@ -105,9 +105,9 @@ session.domOverlayState = {
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load({url: textures[index]});
     const material = new THREE.MeshStandardMaterial({ map: texture });
-const dracoLoader = new THREE.DRACOLoader();
-    dracoLoader.setDecoderPath('./draco/'); // Set the path to the Draco decoder files
-    loader.setDecoderPath(dracoLoader);
+// const dracoLoader = new THREE.DRACOLoader();
+//     dracoLoader.setDecoderPath('./draco/'); // Set the path to the Draco decoder files
+//     loader.setDecoderPath(dracoLoader);
    
     loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", function (gltf) {
         reticle = gltf.scene;

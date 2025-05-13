0bbc103d1c567635a2855da362df1e76e96ee619
DialC <mrchrismugridge@gmail.com>
2025-05-13 22:10:15 +0100

    Update qrHandler.js

--- qrHandler.js
+++ qrHandler.js
@@ -14,7 +14,7 @@ function startScanning() {
       aspect = innerWidth / innerHeight;
     }
    
-    html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox:{width:350, height:350}, aspectRatio: aspect}, onScanSuccess);
+    html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox:{width:innerWidth*0.8, height:innerWidth*0.8}, aspectRatio: aspect}, onScanSuccess);
     }
   
   function onScanSuccess(qrCodeMessage) {
